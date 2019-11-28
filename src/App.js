6604d3c2f8f7ae5b05b0488803edf119b94f2b71
holgermanzke <manzkeholger@gmail.com>
2019-11-28 14:12:13 +0100

    Add basic content and layout

--- src/App.js
+++ src/App.js
@@ -4,21 +4,48 @@ import { ThemeProvider } from "emotion-theming";
 import { themes } from "./themes/themes";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import BottomNavigation from "./components/BottomNavigation";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const Header = styled.div`
+  height: 60px;
+  background: ${props => props.theme.secondary};
+`;
+
+const Main = styled.main`
+  flex-grow: 1;
+`;
+
+const Footer = styled.footer`
+  height: 60px;
+`;
 
 function App() {
   return (
     <ThemeProvider theme={themes.default}>
       <GlobalStyles />
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <div>APP</div>
-          </Route>
-          <Route exact path="/new">
-            <div>New</div>
-          </Route>
-        </Switch>
-        <BottomNavigation />
+        <Container>
+          <Header>Aplicants DB</Header>
+          <Main>
+            <Switch>
+              <Route exact path="/">
+                <div>APP</div>
+              </Route>
+              <Route exact path="/new">
+                <div>New</div>
+              </Route>
+            </Switch>
+          </Main>
+          <Footer>
+            <BottomNavigation />
+          </Footer>
+        </Container>
       </Router>
     </ThemeProvider>
   );
--- src/components/BottomNavigation.js
+++ src/components/BottomNavigation.js
@@ -1,16 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styled from "@emotion/styled";
 
 export default function BottomNavigation() {
-  const Nav = styled.div`
+  const location = useLocation();
+  const Container = styled.div`
     display: flex;
+    justify-content: space-around;
+    height: 100%;
+  `;
+  const NavigationLink = styled(Link)`
+    flex-grow: 1;
+    text-decoration: none;
+    background: ${props =>
+      props.active ? props.theme.text : props.theme.secondary};
+    color: ${props =>
+      props.active ? props.theme.background : props.theme.text};
+    text-transform: uppercase;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.4rem;
+    font-weight: 600;
   `;
 
   return (
-    <Nav>
-      <Link to="/">Home</Link>
-      <Link to="/new">New</Link>
-    </Nav>
+    <Container>
+      <NavigationLink to="/" active={location.pathname === "/"}>
+        Home
+      </NavigationLink>
+      <NavigationLink to="/new" active={location.pathname === "/new"}>
+        New
+      </NavigationLink>
+    </Container>
   );
 }
